ddf28a24772aa001daf1bf85a8a8fbfc306e27ca
Advitya Dua <duaadvitya@gmail.com>
2026-06-18 21:13:52 +0530

    Fixed bug

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    bodyParser: true,
+    bodyParser: false,
   });
 
   app.enableShutdownHooks();
